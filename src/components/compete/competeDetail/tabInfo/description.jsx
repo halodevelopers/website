09242ec3f02160822f3c75356437833653126795
atwanzireT <tatwanzire@gmail.com>
2022-04-04 16:51:35 +0300

    working on compete tabs

---
+++ src/components/compete/competeDetail/tabInfo/description.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class DescriptionDetail extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+                <h1>Description</h1>
+                The objective of this competition is to create a machine learning model to help Kenyan
+                     non-profit organization Local Ocean Conservation anticipate the number of turtles they will 
+                     rescue from each of their rescue sites as part of their By-Catch Release Programme.
+
+                    The data used to train the model will be historic data on the number of turtles rescued from each site from 1998 until 2018. To date, Local Ocean Conservation has released over 10,000 sea turtles.
+
+                    An accurate prediction will enable Local Ocean Conservation to allocate staff and resources more efficiently.
+
+                    About Local Ocean Conservation (LOC) (localocean.co):
+
+                    LOC is a private, not-for-profit organisation committed to the protection of Kenya’s marine environment. LOC supports the communities and coastal areas in Watamu and Diani, Kilifi County with marine conservation and community development projects – centered around a holistic approach to conservation. LOC has been doing marine conservation for over 20 years.
+            </div>
+        );
+    }
+}
+
+export default DescriptionDetail;
--- src/components/compete/competeDetail/tabInfo/info.jsx
+++ src/components/compete/competeDetail/tabInfo/info.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Info from './tabInfo/info';
+import DescriptionDetail from './description';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -15,8 +14,8 @@ function TabPanel(props) {
     <div
       role="tabpanel"
       hidden={value !== index}
-      id={`scrollable-auto-tabpanel-${index}`}
-      aria-labelledby={`scrollable-auto-tab-${index}`}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
       {...other}
     >
       {value === index && (
@@ -36,20 +35,24 @@ TabPanel.propTypes = {
 
 function a11yProps(index) {
   return {
-    id: `scrollable-auto-tab-${index}`,
-    'aria-controls': `scrollable-auto-tabpanel-${index}`,
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
   };
 }
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    width: '100%',
     backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: 224,
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
   },
 }));
 
-export default function CompeteTab() {
+export default function Info() {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
@@ -59,33 +62,42 @@ export default function CompeteTab() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="scrollable"
-          scrollButtons="auto"
-          aria-label="scrollable auto tabs example"
-        >
-          <Tab label="Info" {...a11yProps(0)} />
-          <Tab label="Data" {...a11yProps(1)} />
-          <Tab label="Discussion" {...a11yProps(2)} />
-          <Tab label="LeaderBoard" {...a11yProps(3)} />
-        </Tabs>
-      </AppBar>
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        className={classes.tabs}
+      >
+        <Tab label="Description" {...a11yProps(0)} />
+        <Tab label="Item Two" {...a11yProps(1)} />
+        <Tab label="Item Three" {...a11yProps(2)} />
+        <Tab label="Item Four" {...a11yProps(3)} />
+        <Tab label="Item Five" {...a11yProps(4)} />
+        <Tab label="Item Six" {...a11yProps(5)} />
+        <Tab label="Item Seven" {...a11yProps(6)} />
+      </Tabs>
       <TabPanel value={value} index={0}>
-        <Info/>
+        <DescriptionDetail/>
       </TabPanel>
       <TabPanel value={value} index={1}>
-        Data
+        Item Two
       </TabPanel>
       <TabPanel value={value} index={2}>
-        Discussion
+        Item Three
       </TabPanel>
       <TabPanel value={value} index={3}>
-        LeaderBoard
+        Item Four
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Item Five
+      </TabPanel>
+      <TabPanel value={value} index={5}>
+        Item Six
+      </TabPanel>
+      <TabPanel value={value} index={6}>
+        Item Seven
       </TabPanel>
     </div>
   );
