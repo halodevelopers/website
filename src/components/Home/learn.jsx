b854e089911cc5c2ae5122209ca5ce975639eddd
kuyesu <2019bce028@std.must.ac.ug>
2022-04-08 15:19:47 +0000

    styling for the learn

--- src/components/Home/learn.jsx
+++ src/components/Home/learn.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import MediaCard from './cards';
+import styles from './css/learn.module.css';
+import clsx from 'clsx';
 
 class LearnSection extends Component {
     state = {}
     render() {
         return (
-            <div className="container">
-                <div>
+            <div className={clsx("container learn", styles.learn)}>
+                <div className={clsx('text-title', styles.text)}>
                     <h1>Learn And Master</h1>
                 </div>
                 <div className="container">
