0898ad655b695e91eaf4ebeb9835a210146678b1
atwanzireT <tatwanzire@gmail.com>
2022-04-09 12:26:12 +0300

    Removed appbar and commented out react-announcementbar

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Halodev from './pages/halodev'
-import { AnnouncementBar } from 'react-announcement-bar';
+// import { AnnouncementBar } from 'react-announcement-bar';
 // import Docusaurus from './docusaurus.config';
 
 ReactDOM.render(
