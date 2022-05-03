f4f6926c47610ae0ddd1e9019eac266b4c3915b2
kuyesu <2019bce028@std.must.ac.ug>
2022-05-02 20:20:23 -0400

    correct error for partnership component

--- src/components/Home/patnership.jsx
+++ src/components/Home/patnership.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/css/bootstrap.css';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import clsx from 'clsx';
+import styles from './css/patnership.module.css';
 
 class Patnership extends Component {
     state = {}
