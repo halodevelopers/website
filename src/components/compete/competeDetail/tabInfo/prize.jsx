4cea117bf70e8d2ffed428fc387b8f5d8e93e88f
atwanzireT <tatwanzire@gmail.com>
2022-05-06 16:03:43 +0300

    working on the font of the website

--- src/components/compete/competeDetail/tabInfo/prize.jsx
+++ src/components/compete/competeDetail/tabInfo/prize.jsx
@@ -4,9 +4,9 @@ class Prize extends Component {
     state = {}
     render() {
         return (
-            <div className='container top-margin'>
-                <h5 id='prize'>Prize</h5>
-                <p>
+            <div className='container top-margin mt-5'>
+                <h5 style={{fontSize:"16px"}} id='prize'>Prize</h5>
+                <p style={{fontSize:"14px"}}>
                     This is a learning competition. Aside from knowledge, there are no prizes for this competition.
                 </p>
             </div>
--- src/components/compete/competeDetail/tabInfo/timeline.jsx
+++ src/components/compete/competeDetail/tabInfo/timeline.jsx
@@ -4,9 +4,9 @@ class Timeline extends Component {
     state = {}
     render() {
         return (
-            <div className='container top-margin'>
-                <h5 id='timeline'>Timeline</h5>
-                <p>
+            <div className='container top-margin mt-5'>
+                <h6 style={{fontSize:"16px"}} id='timeline'>Timeline</h6>
+                <p style={{fontSize:"14px"}}>
                     As this is a knowledge competition it will not close.
                     We reserve the right to update the contest timeline if necessary.
                 </p>
