bc4fad9995e297f3d25125a41c3cd03468d545ad
atwanzireT <tatwanzire@gmail.com>
2022-05-08 07:53:43 +0300

    working on edit profile btn

--- src/components/DashBoard/userCard.jsx
+++ src/components/DashBoard/userCard.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import avatar from "./images/avatar.jpg";
 import clsx from "clsx";
@@ -10,30 +10,30 @@ class UserCard extends Component {
   state = {};
   render() {
     return (
-      <div className={clsx("container-fluid mt-5")}>
-        <div className={clsx(styles.header)}>
-          <div className={clsx(styles.hero)}>
-            <div className={clsx(styles.avatar_div)}>
-              <img
-                src={avatar}
-                className={clsx("rounded-circle", styles.avatar)}
-                style={{ width: "180px" }}
-                alt="..."
-                align="center"
-              ></img>
+      <Fragment>
+        <div className={clsx("mt-5")}>
+          <div className={clsx(styles.header)}>
+            <div className={clsx(styles.hero)}>
+              <div className={clsx(styles.avatar_div)}>
+                <img
+                  src={avatar}
+                  className={clsx("rounded-circle", styles.avatar)}
+                  style={{ width: "180px" }}
+                  alt="..."
+                  align="center"
+                ></img>
+              </div>
             </div>
           </div>
-        </div>
-        <hr />
-        <div className="mt">
-          <div className="container-fluid">
-            <div className={clsx("mt-5",)}>
-              <DashboardTab />
-            </div>
+          <div>
           </div>
         </div>
-
-      </div>
+        <div>
+          <div>
+            <DashboardTab />
+          </div>
+        </div>
+      </Fragment>
 
     );
   }
