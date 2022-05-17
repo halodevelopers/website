8c632971d650e0cd13a6e2454053344bf421a6ed
kuyesu <2019bce028@std.must.ac.ug>
2022-05-17 07:43:58 -0400

    desc

--- src/components/Home/home/description.jsx
+++ src/components/Home/home/description.jsx
@@ -11,7 +11,9 @@ const Description = () => {
             <div className="row">
                 <div className="col-md-12">
                     <p className={clsx("text-center", styles.text)}  >
-                        Halodevs' mission is to build safe artificial general intelligence (AGI) which benefits all of humanity, working to solve the world’s most pressing challenges using machine learning and AI. We are builders who care deeply about real-world implications and applications. We connect data scientists with organisations, and provide a place to learn, hone your skills and find a job. We want to transform the globe & African continent and showcase African data science talent & the power of opensource to the world
+                        Halodevs' mission is to build safe artificial general intelligence (AGI) which benefits all of humanity, working to solve the world’s most pressing challenges using machine learning and AI. We are builders who care deeply about real-world implications and applications. We want to transform the globe, and showcase our talent & the power of opensource to the world
+
+                        {/*                         Halodevs' mission is to build safe artificial general intelligence (AGI) which benefits all of humanity, working to solve the world’s most pressing challenges using machine learning and AI. We are builders who care deeply about real-world implications and applications.  We connect data scientists with organisations, and provide a place to learn, hone your skills and find a job. We want to transform the globe & African continent and showcase African data science talent & the power of opensource to the world*/}
                     </p>
                 </div>
             </div>
