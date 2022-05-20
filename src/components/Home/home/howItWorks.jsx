e02025785791cbfa92a78f65d4e4aac1a0306343
kuyesu <2019bce028@std.must.ac.ug>
2022-05-20 14:03:56 -0400

    update the works

--- src/components/Home/home/howItWorks.jsx
+++ src/components/Home/home/howItWorks.jsx
@@ -84,7 +84,7 @@ export default function HowItWorks() {
             {activeStep === steps.length ? (
                 <React.Fragment>
                     <Typography sx={{ mt: 2, mb: 1 }}>
-                        All steps completed - you&apos;re finished
+                        You have completed all the steps - you&apos;re finished
                     </Typography>
                     <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
                         <Box sx={{ flex: '1 1 auto' }} />
