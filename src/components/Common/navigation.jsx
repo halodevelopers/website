914da106866b459827a370805b23ced19d4fc100
kuyesu <kuyeso@halodevs.tech>
2022-06-21 03:34:14 -0400

    avatar

--- src/components/Common/navigation.jsx
+++ src/components/Common/navigation.jsx
@@ -7,6 +7,11 @@ import styles from './css/navbar.module.css';
 import clsx from 'clsx';
 import AnnouncementBar from './anouncementbar';
 import style from './css/register.module.css';
+import {
+    Avatar,
+    Stack,
+  } from '@mui/material';
+  
 
 import { useSelector, useDispatch } from 'react-redux';
 import { logout, reset } from '../../features/auth/authSlice';
@@ -14,78 +19,81 @@ import { logout, reset } from '../../features/auth/authSlice';
 function NavBar() {
     const naviaget = useNavigate()
     const dispatch = useDispatch()
-    const { user } = useSelector((state) => state.auth )
+    const { user } = useSelector((state) => state.auth)
 
     const onLogout = () => {
         dispatch(logout())
         dispatch(reset())
         naviaget('/')
     }
-    
-        return (
-            <div>
-                <AnnouncementBar />
-                <div className={clsx("container-fluid sticky-top", styles.navbar)}>
 
-                    <nav className={clsx("navbarlight navbar navbar-expand-lg navbar-light p-2")}>
+    return (
+        <div>
+            <AnnouncementBar />
+            <div className={clsx("container-fluid sticky-top", styles.navbar)}>
 
-                        <div className="container-fluid">
-                            <Link className={clsx("navbarlink", styles.link)} to="/">
-                                <a className={clsx("", styles.navbarbrand)} id='logo' href="/" />
-                                <a className={clsx("", styles.navbartext)} id='logo' href="/">Halodevs</a>
-                            </Link>
-                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                                <span className="navbar-toggler-icon"></span>
-                            </button>
-                            <div className="nav-inner collapse navbar-collapse justify-content-center" id="navbarNav">
-                                <ul className="navbar-nav">
-                                    <li className="nav-item">
-                                        <Link className={clsx(styles.link)} to="/compete">
-                                            <a className="nav-link" href="#">Compete</a>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className={clsx(styles.link)} to="/learn">
-                                            <a className="nav-link" href="#">Learn</a>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className={clsx(styles.link)} to="/community">
-                                            <a className="nav-link" href="#">Leaderboard</a>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className={clsx(styles.link)} to="/projects">
-                                            <a className="nav-link" href="#">Projects</a>
-                                        </Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className={clsx(styles.link)} to="/jobs">
-                                            <a className="nav-link">Jobs</a>
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div className="collapse d-flex justify-content-end" id="navbarNav">
-                               { user ? (
-                                
-                                <button className={("btn btn-outline-light", style.btn)} onClick={onLogout}>Logout</button>
+                <nav className={clsx("navbarlight navbar navbar-expand-lg navbar-light p-2")}>
+
+                    <div className="container-fluid">
+                        <Link className={clsx("navbarlink", styles.link)} to="/">
+                            <a className={clsx("", styles.navbarbrand)} id='logo' href="/" />
+                            <a className={clsx("", styles.navbartext)} id='logo' href="/">Halodevs</a>
+                        </Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="nav-inner collapse navbar-collapse justify-content-center" id="navbarNav">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <Link className={clsx(styles.link)} to="/compete">
+                                        <a className="nav-link" href="#">Compete</a>
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className={clsx(styles.link)} to="/learn">
+                                        <a className="nav-link" href="#">Learn</a>
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className={clsx(styles.link)} to="/community">
+                                        <a className="nav-link" href="#">Leaderboard</a>
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className={clsx(styles.link)} to="/projects">
+                                        <a className="nav-link" href="#">Projects</a>
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className={clsx(styles.link)} to="/jobs">
+                                        <a className="nav-link">Jobs</a>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="collapse d-flex justify-content-end" id="navbarNav">
+                            {user ? (
+
+                                <Stack direction="row" alignItems="center" justifyContent="center" spacing={2} sx={{ paddingBottom: 0 }}>
+                                    <Avatar alt="Cindy Baker" src="https://64.media.tumblr.com/6430bbf22c54bd7446a96be8af5ec0a5/94d97124995c74f1-59/s400x600/87501bd52c57ba8b2aad5cd5ac0c722db51923c6.pnj" sx={{ width: 50, height: 50 }} />
+                                    <button className={("btn btn-outline-light", style.btn)} onClick={onLogout}>Logout</button>
+                                </Stack>
                                 // avatar
-                                
-                                
-                               ) : (
-                                    <>
+
+
+                            ) : (
+                                <>
                                     <RegisterModal />
                                     <LoginModal />
-                                    </>
-                               ) }
-                                
-                            </div>
+                                </>
+                            )}
+
                         </div>
-                    </nav>
-                </div>
+                    </div>
+                </nav>
             </div>
-        )
+        </div>
+    )
 }
 
 export default NavBar;
