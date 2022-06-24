0874c895d6ccb0c78ceef2cdbac573af1b62ef43
atwanzireT <tatwanzire@gmail.com>
2022-06-24 09:24:52 +0300

    added lorem to about page

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -5,6 +5,20 @@ function About() {
     return ( 
         <div>
             <NavBar/>
+            <div className="container">
+                <div>
+                    <h1>What is Halodev</h1>
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, reiciendis, rerum inventore, tenetur quam nisi ipsum omnis maxime architecto veritatis esse quo minima molestias! Accusamus tempore temporibus non exercitationem iusto?</p>
+                </div>
+                <div>
+                    <h1>Who are Halodev</h1>
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, reiciendis, rerum inventore, tenetur quam nisi ipsum omnis maxime architecto veritatis esse quo minima molestias! Accusamus tempore temporibus non exercitationem iusto?</p>
+                </div>
+            </div>
         </div>
      );
 }
