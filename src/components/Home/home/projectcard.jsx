0bc5c6cd72c5b31451bbab0de2ba3e44e9585725
atwanzireT <tatwanzire@gmail.com>
2022-06-25 23:49:38 +0300

    worked on project cards

--- src/components/Home/home/projectcard.jsx
+++ src/components/Home/home/projectcard.jsx
@@ -10,17 +10,17 @@ function ProjectCard() {
         <Fragment>
             <div className={clsx("card m-2", styles.card)} >
                 <div className="row">
-                    <div className="col-auto">
+                    <div className={clsx("col-auto", styles.col)}>
                         <img src={image1} alt="" srcset="" className={clsx('img roundedCircle', styles.img)} />
                     </div>
-                    <div className="col-6 m-2">
-                        <Link className={clsx(styles.link)} to="/projects/detail">
-                            <strong className={clsx(styles.title)}>Brain Health Detection <span className={clsx("d-inline badge m-2", styles.badge)} style={{ backgroundColor: "#0B0B45" }}>Health</span>
-                            <span className={clsx("d-inline badge m-2", styles.badge)} style={{ backgroundColor: "#fcb70f" }}>Sopnsor this project</span>
+                    <div className={clsx("col-6 m-2", styles.col)}>
+                        <Link className={clsx(styles.link, styles.col)} to="/projects/detail">
+                            <strong className={clsx(styles.title, styles.col)}>Brain Health Detection <span className={clsx("d-inline badge m-2", styles.col, styles.badge)} style={{ backgroundColor: "#0B0B45" }}>Health</span>
+                            <span className={clsx("d-inline badge m-2", styles.title, styles.badge)} style={{ backgroundColor: "#fcb70f" }}>Sponsor this project</span>
                             </strong>
                             <p>In brain tumor classification using machine learning, we built a binary classifier to detect brain tumors from MRI scan images.</p>
                         </Link>
-                        <div>
+                        <div className={clsx(styles.col)}>
                             <p className={clsx("d-inline badge m-2", styles.badge)} style={{ backgroundColor: "#c8c8e6", color: "#0B0B45" }}>Python</p>
                             <p className={clsx("d-inline badge m-2", styles.badge)} style={{ backgroundColor: "#c8c8e6", color: "#0B0B45" }}>TensorFlow</p>
                             <p className={clsx("d-inline badge m-2", styles.badge)} style={{ backgroundColor: "#c8c8e6", color: "#0B0B45" }}>Keras</p>
@@ -29,7 +29,7 @@ function ProjectCard() {
                             <p className={clsx("d-inline badge m-2", styles.badge)} style={{ backgroundColor: "#c8c8e6", color: "#0B0B45" }}>sklearn</p>
                         </div>
                     </div>
-                    <div className={clsx("col-2 m-2")}>
+                    <div className={clsx("col-2 m-2", styles.col)}>
                         <div className={clsx("data justitify-content-end", styles.next)}>
                             <h6>Contributors <Badge bg="secondary">25</Badge></h6>
                             <h6>Mentors <Badge bg="secondary">04</Badge></h6>
