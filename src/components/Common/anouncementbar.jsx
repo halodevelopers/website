efe53cf8e139e83397c6278ac3cf06befc1905ef
atwanzireT <tatwanzire@gmail.com>
2022-06-27 21:46:28 +0300

    annoucement bar

--- src/components/Common/anouncementbar.jsx
+++ src/components/Common/anouncementbar.jsx
@@ -8,14 +8,11 @@ class AnnouncementBar extends Component {
     state = {}
     render() {
         return (
-            <div className={clsx("container", styles.container)}>
+            <div className={clsx("container-fluid", styles.container)}>
                 <div className="row">
-                    <div className="col-md-12">
-
+                    <div className="col-md-12 text-center ">
                         {/* support ukrain banner */}
                         <div class="announcementBarContent_hd8n">Support Ukraine 🇺🇦 <a target="_blank" rel="noopener noreferrer" href="https://opensource.facebook.com/support-ukraine"> Help Provide Humanitarian Aid to Ukraine</a>.</div>
-
-
                     </div>
                 </div>
             </div>
